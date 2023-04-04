df7f8cf0aab9ef725136c4dea69ccc2b40cc78c2
ytyaru <yttry0@gmail.com>
2023-04-04 10:23:06 +0900

    ライセンス名一覧作成

--- docs/0/js/main.js
+++ docs/0/js/main.js
@@ -15,7 +15,11 @@ window.addEventListener('DOMContentLoaded', async(event) => {
     // cd gitignore
     // find -maxdepth 1 -name '*.gitignore' | sort | sed 's/^\.\///g' | sed 's/\.gitignore//g' | sed "s/^/'/g" |  sed "s/$/'/g" | tr '\n', ',' | sed 's/,$//g'
     const langs = ['AL','Actionscript','Ada','Agda','Android','AppEngine','AppceleratorTitanium','ArchLinuxPackages','Autotools','C++','C','CFWheels','CMake','CUDA','CakePHP','ChefCookbook','Clojure','CodeIgniter','CommonLisp','Composer','Concrete5','Coq','CraftCMS','D','DM','Dart','Delphi','Drupal','EPiServer','Eagle','Elisp','Elixir','Elm','Erlang','ExpressionEngine','ExtJs','Fancy','Finale','FlaxEngine','ForceDotCom','Fortran','FuelPHP','GWT','Gcov','GitBook','Go','Godot','Gradle','Grails','Haskell','IGORPro','Idris','JBoss','JENKINS_HOME','Java','Jekyll','Joomla','Julia','KiCad','Kohana','Kotlin','LabVIEW','Laravel','Leiningen','LemonStand','Lilypond','Lithium','Lua','Magento','Maven','Mercury','MetaProgrammingSystem','Nanoc','Nim','Node','OCaml','Objective-C','Opa','OpenCart','OracleForms','Packer','Perl','Phalcon','PlayFramework','Plone','Prestashop','Processing','PureScript','Python','Qooxdoo','Qt','R','ROS','Racket','Rails','Raku','RhodesRhomobile','Ruby','Rust','SCons','Sass','Scala','Scheme','Scrivener','Sdcc','SeamGen','SketchUp','Smalltalk','Stella','SugarCRM','Swift','Symfony','SymphonyCMS','TeX','Terraform','Textpattern','TurboGears2','TwinCAT3','Typo3','Unity','UnrealEngine','VVVV','VisualStudio','Waf','WordPress','Xojo','Yeoman','Yii','ZendFramework','Zephir']
-   
+    // 0-4-2. ライセンス名一覧作成
+    // git clone https://github.com/github/choosealicense.com
+    // cd choosealicense.com/_licenses
+    // find -maxdepth 1 -name '*.txt' | sort | sed 's/^\.\///g' | sed 's/\.txt//g' | sed "s/^/'/g" |  sed "s/$/'/g" | tr '\n', ',' | sed 's/,$//g'
+    const license = ['0bsd','afl-3.0','agpl-3.0','apache-2.0','artistic-2.0','bsd-2-clause','bsd-3-clause-clear','bsd-3-clause','bsd-4-clause','bsl-1.0','cc-by-4.0','cc-by-sa-4.0','cc0-1.0','cecill-2.1','cern-ohl-p-2.0','cern-ohl-s-2.0','cern-ohl-w-2.0','ecl-2.0','epl-1.0','epl-2.0','eupl-1.1','eupl-1.2','gfdl-1.3','gpl-2.0','gpl-3.0','isc','lgpl-2.1','lgpl-3.0','lppl-1.3c','mit-0','mit','mpl-2.0','ms-pl','ms-rl','mulanpsl-2.0','ncsa','odbl-1.0','ofl-1.1','osl-3.0','postgresql','unlicense','upl-1.0','vim','wtfpl','zlib']
     document.getElementById('create').addEventListener('click', async(event) => {
         // バリデートする。パラメータを取得する。引数JSONを作る。fetchでpostする。結果を表示する。
         // 1. パラメータを取得する
